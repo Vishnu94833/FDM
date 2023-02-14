33a22a7f2b54f54be038666fec7279d39d28923f
vkuppan <vkuppan@manh.com>
2023-02-14 16:07:33 +0530

    initial commit

---
+++ src/app/pages/instructions/instructions.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InstructionsRoutingModule } from './instructions-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InstructionsRoutingModule
+  ]
+})
+export class InstructionsModule { }
